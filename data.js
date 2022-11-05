6c486fbfa6747e2f0087f8a83d088b84300a47dc
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2022-11-05 22:39:55 +0000

    add more dogs

--- data.js
+++ data.js
@@ -19,10 +19,40 @@ const dogs = [
         phone: '987-654-321'
     },
     {
+        name: "Guy",
+        avatar: "images/dog-guy.jpg",
+        age: 43,
+        bio: "I'll show you my bike",
+        hasBeenSwiped: false,
+        hasBeenLiked: false,
+        shelter: 'Animal Welfare Charity',
+        phone: '353-454-242'
+    },
+    {
         name: "Teddy",
         avatar: "images/dog-teddy.jpg",
         age: 30,
         bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false,
+        shelter: 'Paws Animal Sanctuary',
+        phone: '123-456-789'
+    },
+    {
+        name: "Rich",
+        avatar: "images/dog-rich.jpg",
+        age: 27,
+        bio: "Let's wine and dine",
+        hasBeenSwiped: false,
+        hasBeenLiked: false,
+        shelter: 'Animal Welfare Charity',
+        phone: '353-454-242'
+    },
+    {
+        name: "Rosmund",
+        avatar: "images/dog-rosmund.jpg",
+        age: 31,
+        bio: "We could be weird together?",
         hasBeenSwiped: false,
         hasBeenLiked: false,
         shelter: 'Paws Animal Sanctuary',
